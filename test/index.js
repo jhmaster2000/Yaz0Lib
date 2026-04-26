b2d28553edbca7fdd587b256989e6e12db5d54d0
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2026-04-25 23:40:13 -0300

    update test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
-const path = require("path");
-const {decompressYaz0File, compressYaz0File} = require("../dist");
+import path from 'node:path';
+import { decompressYaz0File, compressYaz0File } from '../dist/index.js';
 
-const outDecompressedPath = decompressYaz0File(path.resolve(__dirname, "compressed_origin.szs"));
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const outDecompressedPath = decompressYaz0File(path.resolve(__dirname, 'compressed_origin.szs'));
 compressYaz0File(outDecompressedPath);
 
-console.log("Done!");
-
+console.log('Done!');
