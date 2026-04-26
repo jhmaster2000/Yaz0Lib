43a04633aad89ca9ab933160ed3d4e511390e6e2
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2026-04-26 03:03:21 -0300

    split entrypoints

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
  This is a js library to handle Yaz0/Yaz1 compression and decompression.
  Ported from Python's libyaz0, by MasterVermilli0n/AboodXD
  */
-import * as fs from 'node:fs';
 
 /**
  * Check if a buffer is a compressed with Yaz.
@@ -213,36 +212,4 @@ export function decompressYaz0(data: Buffer): Buffer {
     if (!isYazCompressed(data)) throw new Error('Not Yaz0 compressed!');
 
     return decompressBuffer(data);
-}
-
-/**
- * Read a file from a path and compress it with Yaz0.
- *
- * @param path The filepath
- * @param alignment=0 The alignment
- * @param level=0 The compression level
- * @returns {string} The output path. Filename: filename + '.compressed' + original file extension
- */
-export function compressYaz0File(path: string, alignment = 0, level = 0): string {
-    const data = fs.readFileSync(path);
-    const compressed = compressYaz0(data, alignment, level);
-    const output = path.replace(/\.[^/.]+$/, '') + '.compressed' + path.slice(path.lastIndexOf('.'));
-    fs.writeFileSync(output, compressed);
-
-    return output;
-}
-
-/**
- * Read a file from a path and decompress it with Yaz0.
- *
- * @param path The filepath
- * @returns {string} The output path. Filename: filename + '.decompressed' + original file extension
- */
-export function decompressYaz0File(path: string): string {
-    const data = fs.readFileSync(path);
-    const decompressed = decompressYaz0(data);
-    const output = path.replace(/\.[^/.]+$/, '') + '.decompressed' + path.slice(path.lastIndexOf('.'));
-    fs.writeFileSync(output, decompressed);
-
-    return output;
 }
---
+++ src/node.ts
@@ -0,0 +1,34 @@
+import * as fs from 'node:fs';
+import { compressYaz0, decompressYaz0 } from './index.js';
+
+/**
+ * Read a file from a path and compress it with Yaz0.
+ *
+ * @param path The filepath
+ * @param alignment=0 The alignment
+ * @param level=0 The compression level
+ * @returns {string} The output path. Filename: filename + '.compressed' + original file extension
+ */
+export function compressYaz0File(path: string, alignment = 0, level = 0): string {
+    const data = fs.readFileSync(path);
+    const compressed = compressYaz0(data, alignment, level);
+    const output = path.replace(/\.[^/.]+$/, '') + '.compressed' + path.slice(path.lastIndexOf('.'));
+    fs.writeFileSync(output, compressed);
+
+    return output;
+}
+
+/**
+ * Read a file from a path and decompress it with Yaz0.
+ *
+ * @param path The filepath
+ * @returns {string} The output path. Filename: filename + '.decompressed' + original file extension
+ */
+export function decompressYaz0File(path: string): string {
+    const data = fs.readFileSync(path);
+    const decompressed = decompressYaz0(data);
+    const output = path.replace(/\.[^/.]+$/, '') + '.decompressed' + path.slice(path.lastIndexOf('.'));
+    fs.writeFileSync(output, decompressed);
+
+    return output;
+}
